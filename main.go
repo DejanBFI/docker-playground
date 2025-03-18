a20a736082d46b208d48b648d0bc345cba9948a5
Giovanni Dejan_099289 <giovanni.dejan@bfi.co.id>
2025-03-18 09:01:19 +0700

    refactor: move to const

--- main.go
+++ main.go
@@ -8,20 +8,26 @@ import (
 	"github.com/h2non/bimg"
 )
 
+const (
+	width  = 120
+	height = 120
+)
+
 func main() {
 	buffer, err := bimg.Read("image.jpg")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
-	newImage, err := bimg.NewImage(buffer).SmartCrop(120, 120)
+	newImage, err := bimg.NewImage(buffer).SmartCrop(width, height)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
 	size, err := bimg.NewImage(newImage).Size()
-	if size.Width == 120 && size.Height == 120 {
-		fmt.Println("The image size is valid")
+	if size.Width != width || size.Height != height {
+		fmt.Println("The image size is invalid")
+		os.Exit(1)
 	}
 
 	b := base64.StdEncoding.EncodeToString(newImage)
